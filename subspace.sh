1f8897c13e6698795b36ca7e2371df389bc0bcd1
oswy-cpu <74738120+oswy-cpu@users.noreply.github.com>
2022-06-09 13:59:17 +0300

    Update subspace.sh

--- subspace.sh
+++ subspace.sh
@@ -17,8 +17,8 @@ fi
 
 cd $HOME
 rm -rf subspace*
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-05
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-05
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-08/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-08
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-08/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-08
 chmod +x subspace*
 mv subspace* /usr/local/bin/
 
